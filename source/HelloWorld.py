6dc40cce8808da24ef81cb5f487020b9a44fe1fc
Khounborinn <32276495+Khounborinn@users.noreply.github.com>
2020-03-21 09:52:26 -0400

    Added HelloWorld to test camera

---
+++ source/HelloWorld.py
@@ -0,0 +1,14 @@
+import pyrealsense2 as rs
+
+import math as m
+
+
+pipe = rs.pipeline()
+profile = pipe.start()
+try:
+  for i in range(0, 100):
+    frames = pipe.wait_for_frames()
+    for f in frames:
+      print(f.profile)
+finally:
+    pipe.stop()
